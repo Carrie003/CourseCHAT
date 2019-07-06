8334957e9d30cc22636221134be5b426585b2b52
carrieshao <carries003@gmail.com>
2019-07-06 16:23:18 -0500

    lesson

--- app/src/main/java/com/example/course_chat/videotutorial/Lesson.java
+++ app/src/main/java/com/example/course_chat/videotutorial/Lesson.java
@@ -1,17 +1,19 @@
 package com.example.course_chat.videotutorial;
 
 
+import android.net.Uri;
+
 import java.util.ArrayList;
-import java.util.Map;
 
 public class Lesson {
 
-    private String videoPath, name, description, dateCreated;
+    private Uri lessonUri;
+    private String name, description, dateCreated;
     private Integer thumbUp, thumbDown;
     private ArrayList<Comment> comments;
 
-    public Lesson(String videoPath, String name, String description, Integer thumbUp, Integer thumbDown, String dateCreated, ArrayList<Comment> comments){
-        this.videoPath = videoPath;
+    public Lesson(Uri lessonUri, String name, String description, Integer thumbUp, Integer thumbDown, String dateCreated, ArrayList<Comment> comments){
+        this.lessonUri = lessonUri;
         this.name = name;
         this.description = description;
         this.thumbUp = thumbUp;
@@ -20,8 +22,12 @@ public class Lesson {
         this.comments = comments;
     }
 
-    public String getVideoPath() {
-        return videoPath;
+//    public Integer getLessonID() {
+//        return lessonID;
+//    }
+
+    public Uri getLessonUri() {
+        return lessonUri;
     }
 
     public String getName() {
