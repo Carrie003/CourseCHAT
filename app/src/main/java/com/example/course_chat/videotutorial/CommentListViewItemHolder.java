003a0072eb9b5523ba4a9a3207b0e18d9601ca4a
carrieshao <carries003@gmail.com>
2019-07-06 16:21:05 -0500

    comment listview item holder

---
+++ app/src/main/java/com/example/course_chat/videotutorial/CommentListViewItemHolder.java
@@ -0,0 +1,15 @@
+package com.example.course_chat.videotutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.course_chat.R;
+
+public class CommentListViewItemHolder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_list_view_item_holder);
+    }
+}
