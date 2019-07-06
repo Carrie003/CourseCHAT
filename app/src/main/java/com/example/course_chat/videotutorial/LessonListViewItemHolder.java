f387fdbd81157d438507f3fb5c38207a030eae06
carrieshao <carries003@gmail.com>
2019-07-06 16:23:52 -0500

    lesson list view item holder

--- app/src/main/java/com/example/course_chat/videotutorial/LessonListViewItemHolder.java
+++ app/src/main/java/com/example/course_chat/videotutorial/LessonListViewItemHolder.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.example.course_chat.R;
 
-public class ClassListViewItemHolder extends AppCompatActivity {
+public class LessonListViewItemHolder extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_class_list_view_item_holder);
+        setContentView(R.layout.activity_lesson_list_view_item_holder);
     }
 }
