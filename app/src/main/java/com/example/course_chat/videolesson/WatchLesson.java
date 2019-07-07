de62aadf74e4a20c4922781203058b32e979a4e8
carrieshao <carries003@gmail.com>
2019-07-07 18:38:04 -0500

    menu

--- app/src/main/java/com/example/course_chat/videolesson/WatchLesson.java
+++ app/src/main/java/com/example/course_chat/videolesson/WatchLesson.java
@@ -1,17 +1,18 @@
-package com.example.course_chat.videotutorial;
+package com.example.course_chat.videolesson;
 
 import android.app.ProgressDialog;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 import com.example.course_chat.R;
+
+import java.util.ArrayList;
 
 public class WatchLesson extends AppCompatActivity {
 
@@ -20,13 +21,15 @@ public class WatchLesson extends AppCompatActivity {
     private ListView commentListView;
     private Button playButton;
     private ProgressDialog progressDialog;
-    private MediaController lessonMC;
+    private MediaController lessonMedia;
+    private CommetListViewAdapter commentAdapter;
+    private ArrayList<Comment> commentArrayList;
 
 
 
     public WatchLesson(){
         lessonVideoView= findViewById(R.id.lessonVideo);
-        commentListView= findViewById(R.id.commentList);
+        commentListView= findViewById(R.id.commentListView);
 //        playButton = findViewById(R.id.playButton);
 
     }
@@ -40,6 +43,7 @@ public class WatchLesson extends AppCompatActivity {
 
 
     }
+
 
 //    public void setUpLesson(){
 //
@@ -63,9 +67,9 @@ public class WatchLesson extends AppCompatActivity {
     public void  setUpLessonUI(){
         try {
 
-            lessonMC = new MediaController(this);
-            lessonMC.setAnchorView(lessonVideoView);
-            lessonVideoView.setMediaController(lessonMC);
+            lessonMedia = new MediaController(this);
+            lessonMedia.setAnchorView(lessonVideoView);
+            lessonVideoView.setMediaController(lessonMedia);
             Lesson videoLesson  = CreateLessons.IDlessonMap.get(LessonListViewAdapter.lessonID);
             Uri videoUri = videoLesson.getLessonUri();
             lessonVideoView.setVideoURI(videoUri);
@@ -77,6 +81,14 @@ public class WatchLesson extends AppCompatActivity {
         }
     }
 
+    public void setUpCommentUI(){
+
+        commentListView = findViewById(R.id.commentListView);
+        commentAdapter = new CommetListViewAdapter(this, commentArrayList);
+        commentListView.setAdapter(commentAdapter);
+
+
+    }
 
 
 
