3531acb04688ebd5e4aebb4fe43f3c78281d1494
carrieshao <carries003@gmail.com>
2019-07-07 18:35:07 -0500

    menu

---
+++ app/src/main/java/com/example/course_chat/discussion/DiscussionListViewItemHolder.java
@@ -0,0 +1,15 @@
+package com.example.course_chat.discussion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.course_chat.R;
+
+public class DiscussionListViewItemHolder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discussion_list_view_item_holder);
+    }
+}
