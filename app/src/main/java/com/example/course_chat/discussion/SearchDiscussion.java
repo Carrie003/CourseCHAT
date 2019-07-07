54e27d8cd723987a9845944ae68d180802aae608
carrieshao <carries003@gmail.com>
2019-07-07 18:35:14 -0500

    menu

--- app/src/main/java/com/example/course_chat/discussion/SearchDiscussion.java
+++ app/src/main/java/com/example/course_chat/discussion/SearchDiscussion.java
@@ -1,21 +1,18 @@
-package com.example.course_chat.videotutorial;
+package com.example.course_chat.discussion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.course_chat.R;
 
-public class WatchLessonHome extends AppCompatActivity {
+public class SearchDiscussion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_watch_lesson_home);
+        setContentView(R.layout.activity_view_discussion);
+
     }
-
-
-
-
 
 
 
