096ccfd5387959dad09a292f95b788a87549bfc8
carrieshao <carries003@gmail.com>
2019-07-07 18:34:49 -0500

    menu

---
+++ app/src/main/java/com/example/course_chat/discussion/DiscussionHome.java
@@ -0,0 +1,17 @@
+package com.example.course_chat.discussion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.course_chat.R;
+
+public class DiscussionHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discussion_home);
+    }
+
+
+}
