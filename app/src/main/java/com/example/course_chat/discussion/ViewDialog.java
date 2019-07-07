5459c075b72084474393fded8d10bab1251b4cf6
carrieshao <carries003@gmail.com>
2019-07-07 18:35:22 -0500

    menu

--- app/src/main/java/com/example/course_chat/discussion/ViewDialog.java
+++ app/src/main/java/com/example/course_chat/discussion/ViewDialog.java
@@ -1,13 +1,15 @@
-package com.example.course_chat;
+package com.example.course_chat.discussion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DiscussAboutCourse extends AppCompatActivity {
+import com.example.course_chat.R;
+
+public class ViewDialog extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discuss_about_course);
+        setContentView(R.layout.activity_view_discussion2);
     }
 }
