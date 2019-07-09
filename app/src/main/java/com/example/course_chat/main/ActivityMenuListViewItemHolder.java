f122742498302a1e0c9343b8cf8430026dbad38f
carrieshao <carries003@gmail.com>
2019-07-09 11:14:52 -0500

    menu

---
+++ app/src/main/java/com/example/course_chat/main/ActivityMenuListViewItemHolder.java
@@ -0,0 +1,15 @@
+package com.example.course_chat.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.course_chat.R;
+
+public class ActivityMenuListViewItemHolder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_list_view_item_holder);
+    }
+}
