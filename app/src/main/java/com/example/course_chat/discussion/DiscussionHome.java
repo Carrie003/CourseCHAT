7e93a9e6c8f2dca6ee45350e884d04a0b626ad34
carrieshao <carries003@gmail.com>
2019-07-09 16:00:55 -0500

    menu

--- app/src/main/java/com/example/course_chat/discussion/DiscussionHome.java
+++ app/src/main/java/com/example/course_chat/discussion/DiscussionHome.java
@@ -1,11 +1,25 @@
 package com.example.course_chat.discussion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.course_chat.R;
+import com.example.course_chat.vocabquiz.BuildVocabCheckMain;
 
 public class DiscussionHome extends AppCompatActivity {
+
+    private Button createDiscussionButton;
+    private Button viewDiscussionButton;
+
+
+
+    public DiscussionHome(){
+        createDiscussionButton = findViewById(R.id.createDiscussionButton);
+        viewDiscussionButton = findViewById(R.id.viewDiscussionButton);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +27,15 @@ public class DiscussionHome extends AppCompatActivity {
         setContentView(R.layout.activity_discussion_home);
     }
 
+
+//    public void goToCreateDiscussion(View view){
+//
+//        Intent createIntent = new Intent(getApplicationContext(),CreateDiscussion.class);
+//        startActivity(createIntent);
+//    }
+//    public void goToViewDiscussion(View view){
+//        Intent viewIntent = new Intent(getApplicationContext(),ViewDiscussion.class);
+//        startActivity(viewIntent);
+//    }
 
 }
