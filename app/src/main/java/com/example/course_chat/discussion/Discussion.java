51c637b6efb8fae90b3cc21742c3d32fe50fd78e
carrieshao <carries003@gmail.com>
2019-07-09 16:00:45 -0500

    menu

--- app/src/main/java/com/example/course_chat/discussion/Discussion.java
+++ app/src/main/java/com/example/course_chat/discussion/Discussion.java
@@ -1,31 +1,42 @@
 package com.example.course_chat.discussion;
 
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
 
 public class Discussion {
 
 //    private Integer id;
 
+   private Integer  userId;
     private String topic;
     private String content;
     private Integer thumbUp;
     private Integer thumbDown;
     private String dateCreated;
-    private ArrayList<String> dialog;
+    private Map<Integer, Reply> idReplyMap;
 
 
 
-    public Discussion( String topic, String content, Integer thumbUp, Integer thumbDown, String dateCreated, ArrayList<String> dialog){
+    public Discussion(Integer userId, String topic, String content, Integer thumbUp, Integer thumbDown, String dateCreated,  Map<Integer, Reply> idReplyMap){
 
 
+      this.userId = userId;
         this.topic = topic;
         this.content = content;
         this.thumbUp = thumbUp;
         this.thumbDown = thumbDown;
         this.dateCreated = dateCreated;
-        this.dialog = dialog;
+        this.idReplyMap = idReplyMap;
 
+    }
+
+    public Integer getUserId() {
+        return userId;
     }
 
     public String getTopic() {
@@ -44,6 +55,14 @@ public class Discussion {
         return thumbDown;
     }
 
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public Map<Integer, Reply> getIdReplyMap() {
+        return idReplyMap;
+    }
+
     public void setThumbUp(Integer thumbUp) {
         this.thumbUp = thumbUp;
     }
@@ -51,4 +70,22 @@ public class Discussion {
     public void setThumbDown(Integer thumbDown) {
         this.thumbDown = thumbDown;
     }
+
+
+    public static Comparator<Discussion> thumbUpComparator = new Comparator<Discussion>() {
+        @Override
+        public int compare(Discussion d1, Discussion d2) {
+            return (d1.getThumbUp().compareTo(d2.getThumbUp()));
+        }
+    };
+
+
+    public static Comparator<Discussion> ReplyComparator = new Comparator<Discussion>() {
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        public int compare(Discussion d1, Discussion d2) {
+            return  (Integer.compare(d1.getIdReplyMap().keySet().size(), d2.getIdReplyMap().keySet().size()));
+        }
+    };
 }
+
