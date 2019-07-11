066092044e8c9abedd654096ee946234904fe3c0
carrieshao <carries003@gmail.com>
2019-07-10 23:19:35 -0500

    lesson update

--- app/src/main/java/com/example/course_chat/videolesson/Lesson.java
+++ app/src/main/java/com/example/course_chat/videolesson/Lesson.java
@@ -2,24 +2,31 @@ package com.example.course_chat.videolesson;
 
 
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import com.example.course_chat.discussion.Discussion;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
 
-public class Lesson {
+public class Lesson  {
 
     private Uri lessonUri;
     private String title, description, dateCreated;
     private Integer thumbUp, thumbDown;
-    private ArrayList<Comment> comments;
+//    private ArrayList<Comment> comments;
+    private Map<String, Comment> idCommentMap;
 
-    public Lesson(Uri lessonUri, String title, String description, Integer thumbUp, Integer thumbDown, String dateCreated, ArrayList<Comment> comments){
+    public Lesson(Uri lessonUri, String title, String description, Integer thumbUp, Integer thumbDown, String dateCreated,Map<String, Comment> idCommentMap ){
         this.lessonUri = lessonUri;
         this.title = title;
         this.description = description;
         this.thumbUp = thumbUp;
         this.thumbDown = thumbDown;
         this.dateCreated = dateCreated;
-        this.comments = comments;
+        this.idCommentMap = idCommentMap;
     }
 
 //    public Integer getLessonID() {
@@ -50,8 +57,24 @@ public class Lesson {
         return thumbDown;
     }
 
-    public ArrayList<Comment> getComments() {
-        return comments;
+    public Map<String, Comment> getIdCommentMap() {
+        return idCommentMap;
     }
+
+    public static Comparator<Lesson> voteComparator = new Comparator<Lesson>() {
+        @Override
+        public int compare(Lesson l1, Lesson  l2) {
+            return (l1.getThumbUp().compareTo(l2.getThumbUp()));
+        }
+    };
+
+
+    public static Comparator<Lesson> commentComparator = new Comparator<Lesson>() {
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        public int compare(Lesson l1, Lesson l2) {
+            return  (Integer.compare(l1.getIdCommentMap().keySet().size(), l2.getIdCommentMap().keySet().size()));
+        }
+    };
 }
 
