58daf02ee2c3d5cefc21638fee514c39189a6768
carrieshao <carries003@gmail.com>
2019-07-10 23:19:51 -0500

    search lesson update

--- app/src/main/java/com/example/course_chat/videolesson/SearchLesson.java
+++ app/src/main/java/com/example/course_chat/videolesson/SearchLesson.java
@@ -2,24 +2,33 @@ package com.example.course_chat.videolesson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Spinner;
 
 import com.example.course_chat.R;
+import com.example.course_chat.discussion.Discussion;
+import com.example.course_chat.discussion.DiscussionListViewAdapter;
+import com.example.course_chat.interfaces.SearchInterface;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class SearchLesson extends AppCompatActivity implements SearchView.OnQueryTextListener {
+public class SearchLesson extends AppCompatActivity implements SearchView.OnQueryTextListener, AdapterView.OnItemSelectedListener, SearchInterface {
 
-    ListView list;
-    LessonListViewAdapter adapter;
-    SearchView lessonSearch;
-    ArrayList<Lesson> arraylist, lessonList;
-    CreateLessons cL;
+    private ListView lessonListView;
+    private  LessonListViewAdapter adapter;
+    private SearchView lessonSearch;
+//    ArrayList<Lesson> arraylist, lessonList;
+    private CreateLessons cL;
+    private Spinner sortingSpinner;
+    private int j;
 
     public SearchLesson(){
-        arraylist = new ArrayList<>();
-        cL = new CreateLessons();
+//        arraylist = new ArrayList<>();
 
     }
     @Override
@@ -27,28 +36,55 @@ public class SearchLesson extends AppCompatActivity implements SearchView.OnQuer
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_lesson_home);
 
+        setUpSearchViews();
 
-        list = (ListView) findViewById(R.id.lessonListView);
+        setUpSortingSpinner();
 
-        lessonList = cL.lessons;
-        for (Lesson lesson: lessonList) {
+        j = 1;
+    }
 
-            arraylist.add(lesson);
-        }
+//    public void setUpSearchLessonViews(){
+//
+//        }
 
+    @Override
+    public void setUpSearchViews() {
+        cL = new CreateLessons();
 
-        adapter = new LessonListViewAdapter(this, arraylist);
+        lessonListView = findViewById(R.id.lessonListView);
 
+        adapter = new LessonListViewAdapter(this,cL.lessons);
 
-        list.setAdapter(adapter);
-
-
-        lessonSearch = (SearchView) findViewById(R.id.lessonSearchView);
+        lessonListView.setAdapter(adapter);
+        lessonSearch =  findViewById(R.id.lessonSearchView);
         lessonSearch.setOnQueryTextListener(this);
+
+    }
+
+    @Override
+    public void setUpSortingSpinner() {
+
+        ArrayAdapter<CharSequence> subjectListAdaptor = ArrayAdapter.createFromResource(this, R.array.sorting, android.R.layout.simple_spinner_item);//TODO change to SearchDiscussion.this if not work
+        subjectListAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sortingSpinner.setAdapter(subjectListAdaptor);
+        sortingSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public List<Lesson> getSortedListByVotes() {
+        Collections.sort(adapter.lessonList, Lesson.voteComparator);
+        return adapter.lessonList;
+    }
+
+//    @Override
+    public List<Lesson> getSortedListByCommentsOrReplies() {
+        Collections.sort(adapter.lessonList, Lesson.commentComparator);
+        return adapter.lessonList;
     }
 
     @Override
     public boolean onQueryTextSubmit(String query) {
+
 
         return false;
     }
@@ -59,4 +95,39 @@ public class SearchLesson extends AppCompatActivity implements SearchView.OnQuer
         adapter.filter(text);
         return false;
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(j<=0){
+
+            if(position == 1){
+                adapter = new LessonListViewAdapter(this, getSortedListByVotes());
+
+                lessonListView.setAdapter(adapter);
+
+            }
+
+            else if(position == 2){
+                adapter = new LessonListViewAdapter(this, getSortedListByCommentsOrReplies());
+
+                lessonListView.setAdapter(adapter);
+
+            }
+
+
+
+
+
+        }
+        j--;
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
 }
