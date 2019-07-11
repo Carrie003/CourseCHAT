a24660f1ed9a51475b400ca15a9d736f2b76ea6e
carrieshao <carries003@gmail.com>
2019-07-10 23:17:40 -0500

    log in page update

--- app/src/main/java/com/example/course_chat/main/LogIn.java
+++ app/src/main/java/com/example/course_chat/main/LogIn.java
@@ -1,9 +1,12 @@
 package com.example.course_chat.main;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.course_chat.R;
 import com.example.course_chat.vocabquiz.BuildVocabCheckMain;
@@ -16,8 +19,22 @@ public class LogIn extends AppCompatActivity {
     public static  Map<String, User> applicationUserMap;
 
 
+
+    private String applicationId;
+
+
+    private TextView userNameTextView;
+    private TextView passwordTextView;
+
+    private User currentUser;
+    private Boolean signedUpSuccessful;
+
+
+
     public LogIn(){
         applicationUserMap = new HashMap<>();
+
+        signedUpSuccessful = false;
 
     }
     @Override
@@ -25,10 +42,47 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
     }
 
+
+    public User getCurrentUser(){
+
+
+
+
+
+            for(User user: SignUp.idUserMap.values()){
+
+                if(user.getUserName().equals(userNameTextView.getText()) && user.getUserName().equals(userNameTextView.getText())){
+                    currentUser = user;
+                    signedUpSuccessful = true;
+                }
+
+
+            }
+
+
+        return currentUser;
+    }
     public void logIn(View view){
+
+        PackageManager packageManager = getPackageManager();
+        try {
+            applicationId = String.valueOf(packageManager.getApplicationInfo("com.example.app", PackageManager.GET_META_DATA));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(signedUpSuccessful){
+            applicationUserMap.put(applicationId, getCurrentUser());
+        }
+        else{
+
+            Toast.makeText(this, "password or username wrong, please try again...", Toast.LENGTH_SHORT).show();
+        }
         Intent intent = new Intent(getApplicationContext(),Menu.class);
         startActivity(intent);
     }
+
+
 }
