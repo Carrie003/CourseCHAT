d04f3004b92f998fc0581478c34100d67cda7d36
carrieshao <carries003@gmail.com>
2019-07-10 23:24:30 -0500

    vocab check update

--- app/src/main/java/com/example/course_chat/vocabquiz/VocabCheck.java
+++ app/src/main/java/com/example/course_chat/vocabquiz/VocabCheck.java
@@ -1,13 +1,21 @@
 package com.example.course_chat.vocabquiz;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import com.example.course_chat.videolesson.Lesson;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class VocabCheck {
-    private String language, dateCreated, description;
+    private String language, title, dateCreated, description;
     private ArrayList<String> vocabList, meaningList;
     private Integer thumbUp, thumbDown;
 
-    public VocabCheck(String language, String description, ArrayList<String> vocabList, ArrayList<String> meaningList,  Integer thumbUp, Integer thumbDown,String dateCreated){
+    public VocabCheck(String title, String language, String description, ArrayList<String> vocabList, ArrayList<String> meaningList,  Integer thumbUp, Integer thumbDown,String dateCreated){
+
+        this.title = title;
         this.language = language;
         this.description = description;
         this.vocabList = vocabList;
@@ -18,8 +26,19 @@ public class VocabCheck {
 
     }
 
+
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
     public String getLanguage(){
         return language;
+    }
+    public String getDescription() {
+        return description;
     }
 
     public ArrayList<String> getVocabList() {
@@ -41,4 +60,14 @@ public class VocabCheck {
     public String getDateCreated() {
         return dateCreated;
     }
+
+
+    public static Comparator<VocabCheck> voteComparator = new Comparator<VocabCheck>() {
+        @Override
+        public int compare(VocabCheck v1, VocabCheck  v2) {
+            return (v1.getThumbUp().compareTo(v2.getThumbUp()));
+        }
+    };
+
+
 }
